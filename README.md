9e74297a0d97245fcb39be2cefb012c1fdc5df57
shareed <shanreed6@gmail.com>
2020-08-26 18:58:46 -0700

    refactored code

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # React-Memory-Game
+[x] Create a Match Game
+[ ] Add timer
+
+
+Match Card Game with 20 cards, 10 differnt cards each card has a match making it 20 cards total 
+####[Concepts](https://medium.com/@cqpro/lets-build-a-matching-card-game-with-react-d8923d915e46)
+- React
+- Hooks
+- React-Bootstrap
+- Arrays
+ - length
+ - reduce
+ - Math.floor(Math.random())
+- [Pure Functions](https://medium.com/javascript-scene/master-the-javascript-interview-what-is-a-pure-function-d1c076bec976)
+
+#Each Card has the folowing
+- way to check to see if two cards clicked match
+- id/unique
+- when card is not clicked state/action/img
+- when card clicked state/action/img
+- boolean value to check if card is clicked or not
+
+## Logic
+- When one card is clicked just turn it over
+- when two cards are clicked, check to see if they match
+    - if they match they leave them clicked/open
+    - if they do not match turn them back over
+- Each card has a match, both will have the same type but different ids
--- game/src/App.js
+++ game/src/App.js
@@ -5,6 +5,8 @@ import mail from './img/mail.jpg'
 import dementors from './img/dementors.jpg'
 import room from './img/room.jpg'
 import wands from './img/wands.jpg'
+import magic from './img/magic.jpg'
+import train from './img/train.jpg'
 import hall from './img/hall.jpg'
 import car from './img/car.jpg'
 import hat from './img/hat.jpg'
@@ -16,30 +18,32 @@ function App() {
 
   const makeCards = () => {
     let id = 0
-    const images = {mail, room, hall, car, alley, wands, hat, dementors}
-    const cards = Object.keys(images).reduce((result, item) => {
-      const getCard = () => ({
-        id: id++,
-        type: item,
+    const images = {magic, train, mail, room, hall, car, alley, wands, hat, dementors}
+    //for each image use the key to create 2 cards to get 20 cards
+    const cards = Object.keys(images).reduce((result, img) => {//reduce each key to a single value
+      const createCard = () => ({
+        id: id++, //generates a unique id for each card
+        name: img,
         cardBack,
-        frontImg: images[item],
-        flipped: false,
+        frontImg: images[img],
+        flipped: false,//set to false so the card will close when not matched
       })
-      return [...result, getCard(), getCard()]
+      return [...result, createCard(), createCard()]//createCard is call twice to create two cards from each image
     }, [])
-    return suffle(cards)
+    return shuffle(cards)
   }
   
-  const suffle = (arr) => {
-    let len = arr.length
-    for (let i = 0; i < len; i++) {
-      let randomIdx = Math.floor(Math.random() * len)
-      let copyCurrent = {...arr[i]}
-      let copyRandom = {...arr[randomIdx]}
-      arr[i] = copyRandom
-      arr[randomIdx] = copyCurrent
+
+  const shuffle = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      let randomIdx = Math.floor(Math.random() * arr.length)//getting a ramdon index from the array
+      let notRandomCopy = {...arr[i]}// not random array
+      let randomCopy = {...arr[randomIdx]} // random array
+      arr[i] = randomCopy//setting the random array 
+      arr[randomIdx] = notRandomCopy
     }
     return arr
+    //should get back an array of indices randomly
   }
 
 
